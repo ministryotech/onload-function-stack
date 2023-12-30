bc4135ffa131d465c4916a73fd3f8c344d97efea
Keith Jackson <temporal-net@live.co.uk>
2023-12-30 18:29:51 +0000

    New version

--- onload-function-stack.js
+++ onload-function-stack.js
@@ -1,8 +1,9 @@
+// noinspection JSUnusedGlobalSymbols
+
 /*
     Onload Function Stack
     ---------------------
     Manages loading additional functions to the onload event.
-
     https://github.com/ministryotech/onload-function-stack
 */
 (function() {
@@ -11,41 +12,48 @@
 
     /**
      * Adds an event to the onload queue.
-     * 
      * @param {function} func The function to add to the queue.
-     * @param {any} param1 The first (optional) parameter to the function.
-     * @param {any} param2 The second (optional) parameter to the function.
+     * @param {?any} param1 The first (optional) parameter to the function.
+     * @param {?any} param2 The second (optional) parameter to the function.
      */
     const addLoadEvent = function(func, param1, param2) {
         if (func === undefined || func === null)
             throw Error(`Unable to load an event for an undefined or null function pointer (from ${Function.caller}).`)
 
-        if (typeof window.onload !== "function") {
-            if (typeof func !== "function") {
+        if (typeof window.onload !== 'function') {
+            if (typeof func !== 'function') {
                 window.onload = func
             } else {
                 window.onload = () => { func(param1, param2) }
             }
         } else {
-            const oldonload = window.onload
-            window.onload = () => {
-                if (oldonload) { oldonload() }
+            const oldOnLoad = window.onload
+            window.onload = (ev) => {
+                if (oldOnLoad) { oldOnLoad(ev) }
                 func(param1, param2)
             }
         }
     }
 
-    const stackManager = {
+    /**
+     * Manages a stack of events that will trigger on page load.
+     * @type {{add: addLoadEvent}}
+     */
+    const StackManager = {
         add: addLoadEvent
     }
 
-    // Exports to the global scope
-    if (typeof define === "function" && define.amd) {
-        define("onload-function-stack", [], function() { return stackManager })
-    } else if (typeof exports === "object") {
-        module.exports = stackManager
+    /*--------------------------------------------------------------------------*/
+
+    // Export library
+    // noinspection JSUnresolvedReference - define check for require.js module support.
+    if (typeof define === 'function' && define.amd) {
+        // noinspection JSUnresolvedReference - define check for require.js module support.
+        define('onload-function-stack', [], function() { return StackManager })
+    } else if (typeof exports === 'object') {
+        module.exports = StackManager
     } else {
-        root.OnLoadStack = stackManager
+        root.OnLoadStack = StackManager
     }
     
 })()
